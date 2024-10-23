6db3a28ebaa6dc1fa26d6adf86c3b0f2d7c6dcb4
DjoninhaA <djonathan8016@gmail.com>
2024-10-23 13:37:47 -0300

    feat: create GET/POST funcionario

--- gestao_funcionarios/views.py
+++ gestao_funcionarios/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse, HttpResponse
+from .models import Funcionario
 
 # Create your views here.
+
+def funcionario_list(request):
+    if request.method == 'GET':
+        funcionarios = Funcionario.objects.all().values('nome', 'email', 'departamento')
+        return JsonResponse(list(funcionarios))
+    
+    elif request.method == 'POST':
+        data = json.loads(request.body)
+        funcionario = Funcionario(nome = data ['nome'], email = data['email'], departamento = data['departamento'])
+        funcionario.save()
+        return JsonResponse({'nome': funcionario.nome, 'email': funcionario.email, 'departamento': funcionario.departamento}, status = 201)
+        
+
+        ## commita como create funcionario
