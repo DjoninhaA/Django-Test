bf39fe82fb2a3a2bb7b9d6742ac0e4ee2ba4c14e
DjoninhaA <djonathan8016@gmail.com>
2024-10-23 11:54:08 -0300

    feat: create models

---
+++ gestao_funcionarios/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Departamento(models.Model):
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'Departamento: {self.nome}'
+
+
+
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Name: {self.nome} | Email: {self.email} | Departamento: {self.departamento.nome}'
+                                    
