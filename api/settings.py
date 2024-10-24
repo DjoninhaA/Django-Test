3afcfc782cc484e7a3dbe4aa780e1f7cc1a9e140
DjoninhaA <djonathan8016@gmail.com>
2024-10-24 14:15:20 -0300

    docs: dependencies rest-framework

--- api/settings.py
+++ api/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gestao_funcionarios'
+    'gestao_funcionarios',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
