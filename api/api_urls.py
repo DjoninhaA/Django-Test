c2265c5f0bf4bb773582c81880eaf37c29bf51eb
DjoninhaA <djonathan8016@gmail.com>
2024-10-24 16:34:26 -0300

    feat: serializers and views funcionarios

--- api/api_urls.py
+++ api/api_urls.py
@@ -1,11 +1,13 @@
 from django.urls import include, path
 
 from rest_framework import routers
-from gestao_departamentos import views
+from gestao_departamentos import views as departamento_views
+from gestao_funcionarios import views as funcionario_views
 
 
 router = routers.DefaultRouter()
-router.register(r'departamento', views.DepartamentoViewSet)
+router.register(r'departamento', departamento_views.DepartamentoViewSet)
+router.register(r'funcionario', funcionario_views.FuncionarioViewSet)
 
 urlpatterns =  [
     path('', include(router.urls))
